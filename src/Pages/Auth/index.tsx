1d858c4e83f9c00c481c50879405d4366b681c01
Praveen Soni <praveen@shinewebservices.com>
2022-07-29 16:12:33 +0530

    first commit

---
+++ src/Pages/Auth/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+const LoginPage = () => {
+  return (
+    <>
+     
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4"></div>
+          <div className="col-md-4">
+          <h1 className="bg-info mt-4 mb-4">Login page</h1>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </div>
+          <div className="col-md-4"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ src/Routes/index.tsx
@@ -0,0 +1,15 @@
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+const LoginPage = React.lazy(() => import("../Pages/Auth"));
+
+const AppRouter: React.FC = (): JSX.Element => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default AppRouter;
