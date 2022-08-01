4f735da4b5bf49bc1c5971728c19e96a6c6e9391
Praveen Soni <praveen@shinewebservices.com>
2022-08-01 12:11:57 +0530

    first commit

--- src/Pages/Auth/index.tsx
+++ src/Pages/Auth/index.tsx
@@ -17,6 +17,7 @@ const LoginPage = () => {
                 <Form.Control type="email" placeholder="Enter email" />
                 <Form.Text className="text-muted">
                   We'll never share your email with anyone else.
+                  
                 </Form.Text>
               </Form.Group>
 
